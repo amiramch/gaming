2a16e0325f4746e3b5361ef91dfe514ed0223886
John Demo <amiram.chocron@snowflake.com>
2024-12-12 13:30:52 +0000

    added line chart

--- user_activity_app.py
+++ user_activity_app.py
@@ -14,7 +14,7 @@ def user_activity(selected_user,start,end):
     fetch_sql = f"""
     select *
     from quickstart.gaming_anomaly.raw_events
-    where user_id = '{selected_user}' and event_time between '{start}' and '{end}';
+    where user_id = '{selected_user}' and event_time between '{start}' and '{end}'
     order by event_time asc
     """
     flow_df = session.sql(fetch_sql).to_pandas()
@@ -43,4 +43,7 @@ if submit:
         st.error("End Date must be after Start Date")
     else:
         flow_df = user_activity(selected_user,start,end)
+        flow_df['date'] = flow_df['EVENT_TIME'].dt.date
+
         st.dataframe(flow_df)
+        st.line_chart(flow_df.groupby('date').size())
